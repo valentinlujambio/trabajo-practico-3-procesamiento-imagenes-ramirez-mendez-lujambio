2f0c4dca77e27c203fbd7f7ba804fd9115ab8b40
Valentín Lujambio <valentin.lujambio@wiener-lab.com>
2026-06-20 15:58:19 -0300

    Feat - Obtuvimos los frames

--- main.py
+++ main.py
@@ -18,7 +18,40 @@ def detectar_movimiento(frames_video):
     pass
 
 
-def fps_and_frames(video_path):
+def obtener_frames(video_path, nombre_video, propiedades):
+    """
+    Obtenemos los frames del video.
+    """
+    frame_number = 0
+    
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        return None
+    
+    carpeta_destino = os.path.join("frames", nombre_video)
+    os.makedirs(carpeta_destino, exist_ok = True)
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+
+        if ret == True:  
+            frame = cv2.resize(frame, dsize=(int(propiedades['width']/3), int(propiedades['height']/3)))
+            cv2.imshow('Frame', frame)
+
+            ruta = os.path.join(carpeta_destino, f"frame_{frame_number}.jpg")
+            if not cv2.imwrite(ruta, frame):
+                print(f"No se pudo guardar {ruta}")
+
+            frame_number += 1
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:  
+            break  
+
+    cap.release()
+    cv2.destroyAllWindows()
+    
+
+def propiedades_video(video_path):
     """
     Vemos los FPS del video
     """
@@ -28,6 +61,8 @@ def fps_and_frames(video_path):
     return {
         "FPS": cap.get(cv2.CAP_PROP_FPS),
         "frames": int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),
+        "width": int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        "height": int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
     }
 
 
@@ -35,12 +70,10 @@ def procesar_videos():
     directorio_videos = "videos"
     videos = [f for f in os.listdir(directorio_videos) if f.endswith(".mp4")]
     for video in videos:
-        caracteristicas = fps_and_frames(os.path.join(directorio_videos, video))
-        if caracteristicas["FPS"] > 10 and caracteristicas["frames"] > 10:
-            # detectar_movimiento(caracteristicas["frames"])
-            print(caracteristicas)
-        else:
-            print("Video no valido")
+        path_video = os.path.join(directorio_videos, video)
+        nombre_video = video.split(".")[0]
+        propiedades = propiedades_video(path_video)
+        obtener_frames(path_video, nombre_video, propiedades)
 
 
 if __name__ == "__main__":
